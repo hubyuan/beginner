4681af25e670141e37884aa5eb6061a3829c4264
wfy <kaliawng@163.com>
2021-08-27 14:57:00 +0800

    'commit'

---
+++ beginner-game/src/main/java/cn/wfy/zhyhc/Eliminate.java
@@ -0,0 +1,9 @@
+package cn.wfy.zhyhc;
+
+/**
+ * @Description TODO
+ * @auther wfy
+ * @since 2021/7/23
+ */
+public class eliminate {
+}
---
+++ beginner-game/src/main/java/cn/wfy/zhyhc/zhyhc.java
@@ -0,0 +1,9 @@
+package cn.wfy;
+
+/**
+ * @Description TODO
+ * @auther wfy
+ * @since 2021/7/23
+ */
+public class zhyhc {
+}
