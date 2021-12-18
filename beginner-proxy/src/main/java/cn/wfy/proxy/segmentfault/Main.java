b0eb213c812f24f812e4357b605dae39b2ba3e0f
wfy <kaliawng@163.com>
2021-12-19 00:21:24 +0800

    http server by netty and http proxy by netty

--- beginner-proxy/src/main/java/cn/wfy/proxy/segmentfault/Main.java
+++ beginner-proxy/src/main/java/cn/wfy/proxy/segmentfault/Main.java
@@ -2,6 +2,6 @@ package cn.wfy.proxy.segmentfault;
 
 public class Main {
     public static void main(String[] args) {
-        Server server = new Server(9976);
+        Server server = new Server(9988);
     }
 }
