0b16781c4ce2b7d61602f04192a155b590ef0439
wfy <kaliawng@163.com>
2021-12-19 00:23:30 +0800

    test token

--- beginner-proxy/src/main/java/cn/wfy/proxy/segmentfault/Main.java
+++ beginner-proxy/src/main/java/cn/wfy/proxy/segmentfault/Main.java
@@ -3,5 +3,6 @@ package cn.wfy.proxy.segmentfault;
 public class Main {
     public static void main(String[] args) {
         Server server = new Server(9988);
+        System.out.println();
     }
 }
